6728bc44f4b48781cd7a9cf4fbdf21c5c344da5c
anita <slater.anita@gmail.com>
2018-09-11 12:41:37 -0400

    [hw0] Completed task 1

---
+++ hw0/hw0.py
@@ -0,0 +1,10 @@
+################################
+## hw0: Anita Slater
+## Data Mining/ Machine Learning
+################################
+
+## 1: 
+def test_print():
+	print("This is a test statement.")
+if __name__=='__main__':
+	test_print()
