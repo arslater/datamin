34e8d17e8c02f6e9208c242355db5e02f8ccd7e0
anita <slater.anita@gmail.com>
2018-09-11 13:14:11 -0400

    [hw0] Completed task 2

--- hw0/hw0.py
+++ hw0/hw0.py
@@ -8,3 +8,18 @@ def test_print():
 	print("This is a test statement.")
 if __name__=='__main__':
 	test_print()
+
+## 2:
+def list_set_length(items_list,items_set):
+	items_list = [1,2,3,4,3,2,1]
+	items_set={1,2,3,4,3,2,1}
+
+	print("The list contains",len(items_list), "items")
+	print("The set  contains",len(items_set), "items")
+
+test_list=[]
+test_set={}
+
+list_set_length(test_list,test_set)
+
+## 3:	
