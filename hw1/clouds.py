b5849c2150640db49d672355e51bd1dc561ef4a2
anita <slater.anita@gmail.com>
2018-10-07 22:19:34 -0400

    [hw1] have the eignenvalues working

--- hw1/clouds.py
+++ hw1/clouds.py
@@ -10,6 +10,7 @@ def center(matrix):
     cols=matrix.shape[1]
     mean=matrix.mean()
 
+    print(matrix.shape)
     while i < rows:
         while j < cols:
             row.append(matrix[i][j]-mean)
@@ -21,5 +22,12 @@ def center(matrix):
 
     return np.matrix(centeredMatrix)
 
+def covar(matrix):
+    print("nah")
+
+def eign(matrix):
+    print("Eigenvalues: ",np.linalg.eigvals(np.resize(matrix,[10,10])))
+
 data = np.loadtxt("cloud.data")
 print(np.mean(center(data)))
+eign(center(data))
