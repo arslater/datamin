5a05d4d65f2a06b0bc43293d085bb08889d5260a
anita <slater.anita@gmail.com>
2018-11-04 15:52:25 -0500

    [hw2] Have CART working and changed functions to get correct probability values

--- hw2/HW2.py
+++ hw2/HW2.py
@@ -1,8 +1,10 @@
 import math
-def entrophy(D):
 
-    X,y = D
-
+def prob(y):
+    """Returns a tuple (c1,c2
+    of the instances with a 1 and 0 respectively
+    of a certain calss occuring
+    """
     c1=0
     c2=0
 
@@ -11,6 +13,39 @@ def entrophy(D):
             c1+=1
         if item == 0:
             c2+=1
+    return(c1,c2)
+def split(D,index,value):
+    """Returns Dy and Dn, tuples of the split"""
+    X, y = D
+
+    xn = []
+    xy = []
+    yn = []
+    yy = []
+
+    ny = 0
+    nn = 0
+
+   # print("Splitting at column:", index, " when value <", value)
+    for i in range(0, len(X) - 1):
+       # print(X[i][index],"??",value)
+        if (X[i][index] <= value):
+            xy.append(X[i][index])
+            yy.append(y[i])
+        else:
+           # print("DING")
+            xn.append(X[i][index])
+            yn.append(y[i])
+
+    Dy = (xy, yy)
+    Dn = (xn, yn)
+
+    return (Dy,Dn)
+def entrophy(D):
+
+    X,y = D
+
+    c1,c2=prob(y)
 
     prob_c1=c1/(c1+c2)
     prob_c2=c2/(c1+c2)
@@ -31,38 +66,23 @@ def IG(D, index, value):
     Returns:
         The value of the Information Gain for the given split
     """
+    Dy,Dn = split(D,index,value)
 
-    X,y = D
+    Xy,yy = Dy  ## Data set of yes's
+    Xn,yn = Dn  ## Data set of No's
 
-    xn = []
-    xy = []
-    yn = []
-    yy = []
-
-    ny = 0
-    nn = 0
-
-    print("Splitting at column:",index," when value <",value)
-    for i in range(0,len(X)-1):
-        if(X[i][index] < value):
-            xy.append(X[i][index])
-            yy.append(y[i])
-        else:
-            xn.append(X[i][index])
-            yn.append(y[i])
-
-    Dy = (xy,yy)
-    Dn = (xn,yn)
-
-    for item in yy:
-        if item == 1:
-            ny += 1
-    for item in yn:
-        if item == 0:
-            nn +=1
+    # nn=0
+    # ny=0
+    #
+    # for item in yy:
+    #     if item == 1:
+    #         ny += 1
+    # for item in yn:
+    #     if item == 0:
+    #         nn +=1
     #print(nn,len(yn),ny,len(yy))
-    return(entrophy(D)-((ny/len(yy)*entrophy(Dy))
-                       +(nn/len(yn)*entrophy(Dn))))
+    return(entrophy(D)-((len(yy)/len(y)*entrophy(Dy))
+                       +(len(yn)/len(y)*entrophy(Dn))))
 def G(D, index, value):
     """Compute the Gini index of a split on attribute index at value
     for dataset D.
@@ -75,7 +95,32 @@ def G(D, index, value):
     Returns:
         The value of the Gini index for the given split
     """
+    Dy,Dn = split(D,index,value)
 
+    Xy,yy=Dy
+    Xn,yn=Dn
+
+    print(Dn)
+
+    yc1,yc2 = prob(yy)
+    nc1,nc2 = prob(yn)
+
+    ## G(DY)
+    prob_c1=yc1/(yc1+yc2)
+    prob_c2=yc2/(yc1+yc2)
+
+    Gdy=1 - ((prob_c1 ** 2) + (prob_c2 ** 2))
+
+    ## G(DN)
+    prob_c1 = nc1 / (nc1 + nc2)
+    prob_c2 = nc2 / (nc1 + nc2)
+
+    Gdn = 1 - ((prob_c1 ** 2) + (prob_c2 ** 2))
+
+   # print("Prob of ys",(len(yy)/len(y)))
+   # print("Prob of ns",(len(yn)/len(y)))
+
+    return(((len(yy)/len(y))*Gdy)+((len(yn)/len(y))*Gdn))
 
 def CART(D, index, value):
     """Compute the CART measure of a split on attribute index at value
@@ -89,7 +134,39 @@ def CART(D, index, value):
     Returns:
         The value of the CART measure for the given split
     """
+    X,y = D
 
+    Dy,Dn = split(D,index,value)
+
+    Xy,yy = Dy
+    Xn,yn = Dn
+
+    coef = 2*(len(yy)/len(y))*(len(yn)/len(y))
+
+    yc1,yc2 = prob(yy)
+    ## Gets the number of c1's and c2's in the yes split
+
+    nc1,nc2 = prob(yn)
+    ## Gets the number of c1s and c2s in class no
+
+    #yc1: The number of c1s in split yes
+    #yc2: The number of c2s in split yes
+    #nc1: The number of c1s in split no
+    #nc2: The number of c2s in split no
+
+    #yy: contains the calsses for each entry in split yes
+    #yn: contains the classes for each entry in split no
+
+    print(yy)
+    print("NUMBER OF 1s IN YEZ",yc1,len(yy))
+
+    Pc1y = (yc1/len(yy))  # prob of c1 in yes
+    Pc1n = (nc1/len(yn))  # prob of c1 in no
+    Pc2y = (yc2/len(yy))  # prob of c2 in yes
+    Pc2n = (nc2/len(yn))  # prob of c2 in no
+
+    cart = coef*(abs(Pc1y-Pc1n)+abs(Pc2y-Pc2n))
+    print(cart)
 
 def bestSplit(D, criterion):
     """Computes the best split for dataset D using the specified criterion
@@ -137,7 +214,10 @@ def load(filename):
     return(X,y)
 
 X,y=load("train.txt")
-IG((X,y),1,28)
+#IG((X,y),1,28)
+#G((X,y),0,0)
+#print(y)
+CART((X,y),0,0)
 #print(entrophy(load("train.txt")))
 def classifyIG(train, test):
     """Builds a single-split decision tree using the Information Gain criterion
